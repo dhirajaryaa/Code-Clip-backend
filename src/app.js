7cd5586bf85bcd0770e8d6f48d9cf4f99acabf08
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-12-03 18:30:23 +0530

    feat: express app middleware configure

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+export const app = express();
+
+//! middleware setup 
+
+// setup json allow on request
+app.use(express.json({
+    limit: process.env.LIMIT,
+
+}));
+
+// setup cors for security 
+app.use(cors({
+    origin: process.env.ORIGIN,
+    credentials: true
+}));
+
+// setup urlencoded 
+app.use(express.urlencoded({
+    limit: process.env.LIMIT,
+    extended: true,
+}))
+
+// setup static folder
+app.use(express.static("public"));
+
+// setup cookieParser 
+app.use(cookieParser());
+
