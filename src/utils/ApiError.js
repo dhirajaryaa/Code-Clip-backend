f295e98596d07d09c2927a1ac3b3e1691a66e1bd
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-12-03 16:31:34 +0530

    feat: create centralized api error with extended nodejs Error class

---
+++ src/utils/ApiError.js
@@ -0,0 +1,21 @@
+export class ApiError extends Error {
+
+    constructor(
+        status,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message);
+        this.status = status;
+        this.data = null;
+        this.message = message;
+        this.errors = errors;
+        this.success = false;
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
