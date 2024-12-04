e0f89119008b4db4f25697e5984a806aac3f5346
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-12-04 20:15:37 +0530

    fix: middleware req fixed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,24 +1,28 @@
-import { AsyncHandler } from "../utils/AsyncHandler";
-import { ApiError } from "../utils/ApiError";
-import {User} from "../models/user.model.js"
+import { AsyncHandler } from "../utils/AsyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js"
 import jwt from "jsonwebtoken";
 
 //  write custom middleware to extract user info using cookies
-export const verifyJWT = AsyncHandler(async (req,res,next)=>{
-    // TODO 
-    // get cookie from req 
-    const Token = req.cookies?.accessToken || req.headers?.["authorization"]?.replace("Bearer ","");
-
-    // check cookie present or not
-    if(!Token){
-        throw new ApiError(401, "Unauthorized Access!");
+export const verifyJWT = AsyncHandler(async (req, res, next) => {
+    try {
+        // TODO 
+        // get cookie from req 
+        const Token = req.cookies?.accessToken || req.headers?.["authorization"]?.replace("Bearer ", "");
+    
+        // check cookie present or not
+        if (!Token) {
+            throw new ApiError(401, "Unauthorized Access!");
+        }
+        // decode cookie and extract user data 
+        const decodedToken = jwt.verify(Token, process.env.ACCESS_TOKEN_SECRET);
+    
+        // fetch user data form database 
+        const user = await User.findById(decodedToken.id).select("-password -refreshToken");
+        // pass user throw request
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, "Invalid or Expired token!");
     }
-    // decode cookie and extract user data 
-    const decodedToken = jwt.verify(Token,process.env.ACCESS_TOKEN_SECRET);
-
-    // fetch user data form database 
-    const user = await User.findById(decodedToken._id).select("-password -refreshToken");
-    // pass user throw request
-    return req.user = user;
-    next();
 });
