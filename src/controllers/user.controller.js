d4cd4548c99ae2e1d13d0d1f8f8b949abcd92dbb
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-12-04 18:53:22 +0530

    feat: user login func. working

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,31 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js"
 import { uploadFileOnCloudinary } from "../services/cloudinary.service.js"
 
+// generate Access token 
+const generateAccessAndRefreshToken = async (userId) => {
+    // find user form id 
+    const user = await User.findById(userId);
+    if (!user) {
+        throw new ApiError(404, "unable to find user!");
+    }
+
+    // generate token 
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+    // update token on user object 
+    if (refreshToken) {
+        user.refreshToken = refreshToken
+    };
+    await user.save();
+    // return token 
+    return { accessToken, refreshToken };
+};
+
+// options for cookie parser security
+let cookieOptions = {
+    httpOnly: true,
+    secure: true
+}
 
 // register user 
 const registerUser = AsyncHandler(async (req, res) => {
@@ -60,8 +85,52 @@ const registerUser = AsyncHandler(async (req, res) => {
         throw new ApiError(500, "Failed to save user data to MongoDB. Please try again later.");
     };
     // return api response 
-    return res.status(201).json(new ApiResponse(201, createdUser,"successfully create user"))
+    return res.status(201).json(new ApiResponse(201, createdUser, "successfully create user"))
 
 });
 
-export { registerUser }
+// login user
+const loginUser = AsyncHandler(async (req, res) => {
+    //! TODO
+    // get data from frontend [email/username and password ]
+    const { email, username, password } = req.body;
+    // check given data null or empty 
+    if (!(email || username)) {
+        throw new ApiError(400, "Email or Username is Required ");
+    }
+    if (!password) {
+        throw new ApiError(400, "Password is Required ");
+    }
+    // check user exists or not 
+    const existsUser = await User.findOne({
+        $or: [{ email }, { username }],
+    }).select("+password");
+
+    if (!existsUser) {
+        throw new ApiError(404, "User Not Found!");
+    }
+    // check password correct or not 
+    const isPasswordValid = await existsUser.isPasswordCorrect(password);
+    if (!isPasswordValid) {
+        throw new ApiError(401, "password not correct ");
+    }
+    // generate accessToken and requestToken 
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(existsUser._id);
+    // remove password and refresh token 
+    const userData = await User.findById(existsUser._id).select("-password -refreshToken");
+    // return res and cookies  
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, cookieOptions)
+        .cookie("refreshToken", refreshToken, cookieOptions)
+        .json(
+            new ApiResponse(200, {
+                user: userData,
+                accessToken,
+                refreshToken
+            }, "User Login successful!")
+        )
+});
+
+export { registerUser, loginUser }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 
 export const userRouter = Router();
 
+// register route 
 userRouter.route("/register").post(
     upload.fields([
         { name: "avatar", maxCount: 1 },
         { name: "coverImage", maxCount: 1 }
     ])
-    , registerUser)
+    , registerUser);
+
+// login route
+userRouter.route("/login").post(loginUser);
